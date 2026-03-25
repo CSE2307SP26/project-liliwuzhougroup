c62079eb3391b3985f05511bdb4029423a68797a
Nick123-afk <junlinw@wustl.edu>
2026-03-25 17:10:14 -0500

    final

---
+++ src/main/CreateAccount.java
@@ -0,0 +1,8 @@
+package main;
+
+public class CreateAccount {
+
+    public BankAccount execute() {
+        return new BankAccount();
+    }
+}
--- src/test/BankAccountTest.java
+++ src/test/BankAccountTest.java
@@ -1,11 +1,9 @@
 package test;
 
 import main.BankAccount;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-
-import org.junit.jupiter.api.Test;
+import main.CreateAccount;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
@@ -25,5 +23,25 @@ public class BankAccountTest {
         } catch (IllegalArgumentException e) {
             //do nothing, test passes
         }
+    }
+    @Test
+    public void testCreateAccount() {
+        CreateAccount ca = new CreateAccount();
+        BankAccount account = ca.execute();
+
+        assertNotNull(account);
+        assertEquals(0, account.getBalance(), 0.01);
+    }
+     @Test
+    public void testInitialBalance() {
+        BankAccount account = new BankAccount();
+        assertEquals(0, account.getBalance(), 0.01);
+    }
+
+    @Test
+    public void testBalanceAfterDeposit() {
+        BankAccount account = new BankAccount();
+        account.deposit(100);
+        assertEquals(100, account.getBalance(), 0.01);
     }
 }
